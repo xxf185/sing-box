3a2088f28f8402ef7b9284f50eda59e8eeb73af4
xxf185 <109097973+xxf185@users.noreply.github.com>
2025-08-26 21:33:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -309,7 +309,7 @@ main() {
     # show welcome msg
     clear
     echo
-    echo "........... $is_core_name script by $author .........."
+    echo "........... sing-box .........."
     echo
 
     # start installing...
